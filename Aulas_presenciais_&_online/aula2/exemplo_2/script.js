8b925359e4616df9c6feb0d78bd636080436ea47
“Gustavo-Rodrigues01” <gustavorodrigues06062@gmail.com>
2026-02-06 14:14:26 -0300

    adicionando aula2

---
+++ Aulas_presenciais_&_online/aula2/exemplo_2/script.js
@@ -0,0 +1,29 @@
+//declaração usando let (valor mutavel)
+let nome = "Gustavo";
+
+//declaração usando const (valor imutavel)
+const nome2 = "Rodrigues";
+
+//Optar por const sempre que possivel
+
+//tipos de dados inteiros:
+let idade = 25;
+let negativo = -15;
+
+//decimais (ponto flutuante):
+let preço = 7.99;
+
+//string
+let primeiroNome = "Gustavo";
+let segundoNome = "Reis";
+
+//concatenação
+let nomeCompleto = primeiroNome +" "+ segundoNome;
+
+//Booleano
+let t = true;
+let f = false;
+
+//operadores aritimeticos
+
+//operadores relacionais

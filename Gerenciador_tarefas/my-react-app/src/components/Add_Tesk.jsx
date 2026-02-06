3f36fffe902b9e640f6c4785b83ce5c0e2e3eaf6
“Gustavo-Rodrigues01” <gustavorodrigues06062@gmail.com>
2026-02-06 15:59:55 -0300

    Adicionando Gerenciador de tarefas incompleto

---
+++ Gerenciador_tarefas/my-react-app/src/components/Add_Tesk.jsx
@@ -0,0 +1,17 @@
+
+function Add_Tesk(){
+    return(
+        <>
+            <div class="
+                flex flex-col items-center
+                max-w-200px
+                bg-blue-400
+                "
+                >
+
+                <h1>Gerenciador de tarefas</h1>
+            </div>
+        </>
+    );
+};
+export default Add_Tesk;

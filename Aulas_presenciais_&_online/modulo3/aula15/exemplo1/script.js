138f9e895adf52459077000bf92f089293d9ae61
“Gustavo-Rodrigues01” <gustavorodrigues06062@gmail.com>
2026-02-10 17:32:09 -0300

    Adicionando aula15

---
+++ Aulas_presenciais_&_online/modulo3/aula15/exemplo1/script.js
@@ -0,0 +1,9 @@
+ //exemplo de objeto
+ const person ={
+    firstName: "joão",
+    lastName: "Paulo",
+    age: "20",
+    eyeColor: "blue"
+ }
+
+console.log(`Nome compreto ${firstName} ${lastName}`)
---
+++ Aulas_presenciais_&_online/modulo3/aula15/exemplo2/script.js
@@ -0,0 +1,9 @@
+let objectABC = {
+    A: 10,
+    B: 21,
+    c:-5
+};
+
+console.log(objectABC["A"]);
+console.log(objectABC.B);
+console.log(objectABC[c]);//error

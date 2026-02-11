dd6d3ae53e0bec3d64a23932be3caf423929de11
“Gustavo-Rodrigues01” <gustavorodrigues06062@gmail.com>
2026-02-11 15:28:42 -0300

    Adicionando aula 29 e motificando arquivo nome do Aulas

---
+++ Aulas/modulo5/aula29/my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Welcome from "./Components/Welcome";
+function App() {
+  
+  return (
+    <>
+      <h1>component basico react</h1>
+      <Welcome/>
+    </>
+  );
+};
+
+export default App;
---
+++ Aulas/modulo5/aula29/my-react-app/src/Components/Welcome.jsx
@@ -0,0 +1,13 @@
+// Componente React básico
+function Welcome() {
+    return (
+        <div>
+            <p>npm create vite@latest my-react-app -- --template react</p>
+            <p>npm install -g create-vite</p>
+            <p>npm install</p>
+            <p>npm run dev</p>
+            <h2>Bem-vindo ao React</h2>
+        </div>
+    );
+};
+export default Welcome;
